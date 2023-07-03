4d87d7963c0473e33208d438a63ae6399e76f3dc
Carlos Albornoz <er.carlos.albornoz@gmail.com>
2023-07-03 01:17:08 -0600

    Adding nvim, git and pentest zsh configs

---
+++ nvim/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,21 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "cpp",
+    "c",
+    "markdown"
+  };
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
